27080886c4e05ed1643ab785bd4c3c6ee149ad5a
detextre4 <detextre4@gmail.com>
2023-04-23 18:10:22 -0400

    loader changed

--- lib/widgets/global_loader.dart
+++ lib/widgets/global_loader.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_detextre4/main.dart';
+import 'package:flutter_detextre4/utils/config/app_config.dart';
 import 'package:flutter_detextre4/utils/config/extensions_config.dart';
 import 'package:flutter_detextre4/widgets/will_pop_custom.dart';
 import 'package:http/http.dart' as http;
@@ -27,10 +28,30 @@ class _GlobalLoader extends StatelessWidget {
 
     return WillPopCustom(
       onWillPop: () => Future.value(false),
-      child: const Scaffold(
+      child: Scaffold(
           backgroundColor: Colors.transparent,
           body: Center(
-            child: CircularProgressIndicator(),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 70,
+                  height: 70,
+                  child: CircularProgressIndicator(
+                    strokeWidth: 8,
+                    color: AppColors.getColor(context, ColorType.secondary),
+                    backgroundColor:
+                        AppColors.getColor(context, ColorType.primary),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  "Processing...",
+                  style: Theme.of(context).primaryTextTheme.titleLarge,
+                ),
+              ],
+            ),
           )),
     );
   }
