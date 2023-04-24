1ec844760d8921ecc9881894bbe1e6467f953178
detextre4 <detextre4@gmail.com>
2023-04-24 12:55:48 -0400

    global loader multipartRequest added

--- lib/widgets/global_loader.dart
+++ lib/widgets/global_loader.dart
@@ -8,22 +8,33 @@ import 'package:http/http.dart' as http;
 class GlobalLoader {
   static void close() => Navigator.pop(globalNavigatorKey.currentContext!);
 
-  static Future<http.Response> init({Future<http.Response>? request}) async =>
+  static Future<http.Response> init({
+    Future<http.Response>? request,
+    http.MultipartRequest? multipartRequest,
+  }) async =>
       await showDialog(
         context: globalNavigatorKey.currentContext!,
-        builder: (context) => _GlobalLoader(request),
+        builder: (context) => _GlobalLoader(request, multipartRequest),
       ) ??
       http.Response.bytes([], 200);
 }
 
 class _GlobalLoader extends StatelessWidget {
-  const _GlobalLoader(this.request);
+  const _GlobalLoader(this.request, this.multipartRequest);
   final Future<http.Response>? request;
+  final http.MultipartRequest? multipartRequest;
 
   @override
   Widget build(BuildContext context) {
-    Future<void> init() async =>
-        request.isExist ? Navigator.pop(context, await request) : null;
+    Future<void> init() async {
+      if (request.isExist) {
+        Navigator.pop(context, await request);
+      } else if (multipartRequest.isExist) {
+        Navigator.pop(context,
+            await http.Response.fromStream(await multipartRequest!.send()));
+      }
+    }
+
     init();
 
     return WillPopCustom(
