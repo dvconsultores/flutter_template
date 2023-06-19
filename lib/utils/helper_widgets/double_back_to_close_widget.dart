846ba647487f8bdc36da956a3c552557127d5425
detextre4 <detextre4@gmail.com>
2023-06-18 22:16:06 -0400

    double back fixed

--- lib/utils/helper_widgets/double_back_to_close_widget.dart
+++ lib/utils/helper_widgets/double_back_to_close_widget.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_detextre4/utils/config/extensions_config.dart';
 import 'package:flutter_detextre4/utils/general/global_functions.dart';
 
-class DoubleBackToCloseWidget extends StatelessWidget {
+class DoubleBackToCloseWidget extends StatefulWidget {
   const DoubleBackToCloseWidget({
     super.key,
     required this.snackBarMessage,
@@ -19,26 +19,33 @@ class DoubleBackToCloseWidget extends StatelessWidget {
   final Duration doubleBackDuration;
 
   @override
+  State<DoubleBackToCloseWidget> createState() =>
+      _DoubleBackToCloseWidgetState();
+}
+
+class _DoubleBackToCloseWidgetState extends State<DoubleBackToCloseWidget> {
+  DateTime? currentBackPressTime;
+
+  @override
   Widget build(BuildContext context) {
     // * Web
-    if (kIsWeb) return child;
-
-    DateTime? currentBackPressTime;
+    if (kIsWeb) return widget.child;
 
     Future<bool> onWillPop() async {
       DateTime now = DateTime.now();
+
       if (currentBackPressTime.isNotExist ||
-          now.difference(currentBackPressTime!) > doubleBackDuration) {
+          now.difference(currentBackPressTime!) > widget.doubleBackDuration) {
         currentBackPressTime = now;
         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-          content: Text(snackBarMessage),
-          duration: doubleBackDuration,
+          content: Text(widget.snackBarMessage),
+          duration: widget.doubleBackDuration,
           backgroundColor: ColorSnackbarState.neutral.color,
         ));
         return false;
       }
 
-      if (onDoubleBack.isExist) return await onDoubleBack!();
+      if (widget.onDoubleBack.isExist) return await widget.onDoubleBack!();
 
       return true;
     }
@@ -47,7 +54,7 @@ class DoubleBackToCloseWidget extends StatelessWidget {
     if (Platform.isAndroid) {
       return WillPopScope(
         onWillPop: onWillPop,
-        child: child,
+        child: widget.child,
       );
     }
 
@@ -57,7 +64,7 @@ class DoubleBackToCloseWidget extends StatelessWidget {
       child: GestureDetector(
         onHorizontalDragUpdate: (details) =>
             (details.delta.dx > 8).inCase(onWillPop),
-        child: child,
+        child: widget.child,
       ),
     );
   }
