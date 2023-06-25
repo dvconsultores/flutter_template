3c01debe5617e44f6bdcd8bfa070b8d4b77e93dd
detextre4 <detextre4@gmail.com>
2023-06-25 13:17:49 -0400

    fixes

--- lib/utils/config/extensions_config.dart
+++ lib/utils/config/extensions_config.dart
@@ -1,6 +1,6 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:developer' as dev;
-import 'dart:developer';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
@@ -313,9 +313,109 @@ extension NullableStringExtension on String? {
 
 // ? String extension
 extension StringExtension on String {
-  DateTime parseToDateTime() => DateTime.parse(this);
+  /// Emit a log event.
+  ///
+  /// This function was designed to map closely to the logging information
+  /// collected by `package:logging`.
+  ///
+  /// - [message] is the log message
+  /// - [time] (optional) is the timestamp
+  /// - [sequenceNumber] (optional) is a monotonically increasing sequence number
+  /// - [level] (optional) is the severity level (a value between 0 and 2000); see
+  ///   the `package:logging` `Level` class for an overview of the possible values
+  /// - [name] (optional) is the name of the source of the log message
+  /// - [zone] (optional) the zone where the log was emitted
+  /// - [error] (optional) an error object associated with this log event
+  /// - [stackTrace] (optional) a stack trace associated with this log event
+  String log({
+    DateTime? time,
+    int? sequenceNumber,
+    int level = 0,
+    String name = '',
+    Zone? zone,
+    Object? error,
+    StackTrace? stackTrace,
+  }) {
+    dev.log(
+      "$this 🔍",
+      time: time,
+      sequenceNumber: sequenceNumber,
+      level: level,
+      name: name,
+      zone: zone,
+      error: error,
+      stackTrace: stackTrace,
+    );
+    return this;
+  }
 
-  File parseBase64ToFile() => File.fromRawPath(base64Decode(this));
+  /// Constructs a new [DateTime] instance based on [formattedString].
+  ///
+  /// Throws a [FormatException] if the input string cannot be parsed.
+  ///
+  /// The function parses a subset of ISO 8601,
+  /// which includes the subset accepted by RFC 3339.
+  ///
+  /// The accepted inputs are currently:
+  ///
+  /// * A date: A signed four-to-six digit year, two digit month and
+  ///   two digit day, optionally separated by `-` characters.
+  ///   Examples: "19700101", "-0004-12-24", "81030-04-01".
+  /// * An optional time part, separated from the date by either `T` or a space.
+  ///   The time part is a two digit hour,
+  ///   then optionally a two digit minutes value,
+  ///   then optionally a two digit seconds value, and
+  ///   then optionally a '.' or ',' followed by at least a one digit
+  ///   second fraction.
+  ///   The minutes and seconds may be separated from the previous parts by a
+  ///   ':'.
+  ///   Examples: "12", "12:30:24.124", "12:30:24,124", "123010.50".
+  /// * An optional time-zone offset part,
+  ///   possibly separated from the previous by a space.
+  ///   The time zone is either 'z' or 'Z', or it is a signed two digit hour
+  ///   part and an optional two digit minute part. The sign must be either
+  ///   "+" or "-", and cannot be omitted.
+  ///   The minutes may be separated from the hours by a ':'.
+  ///   Examples: "Z", "-10", "+01:30", "+1130".
+  ///
+  /// This includes the output of both [toString] and [toIso8601String], which
+  /// will be parsed back into a `DateTime` object with the same time as the
+  /// original.
+  ///
+  /// The result is always in either local time or UTC.
+  /// If a time zone offset other than UTC is specified,
+  /// the time is converted to the equivalent UTC time.
+  ///
+  /// Examples of accepted strings:
+  ///
+  /// * `"2012-02-27"`
+  /// * `"2012-02-27 13:27:00"`
+  /// * `"2012-02-27 13:27:00.123456789z"`
+  /// * `"2012-02-27 13:27:00,123456789z"`
+  /// * `"20120227 13:27:00"`
+  /// * `"20120227T132700"`
+  /// * `"20120227"`
+  /// * `"+20120227"`
+  /// * `"2012-02-27T14Z"`
+  /// * `"2012-02-27T14+00:00"`
+  /// * `"-123450101 00:00:00 Z"`: in the year -12345.
+  /// * `"2002-02-27T14:00:00-0500"`: Same as `"2002-02-27T19:00:00Z"`
+  ///
+  /// This method accepts out-of-range component values and interprets
+  /// them as overflows into the next larger component.
+  /// For example, "2020-01-42" will be parsed as 2020-02-11, because
+  /// the last valid date in that month is 2020-01-31, so 42 days is
+  /// interpreted as 31 days of that month plus 11 days into the next month.
+  ///
+  /// To detect and reject invalid component values, use
+  /// [DateFormat.parseStrict](https://pub.dev/documentation/intl/latest/intl/DateFormat/parseStrict.html)
+  /// from the [intl](https://pub.dev/packages/intl) package.
+  DateTime toDateTime() => DateTime.parse(this);
+
+  /// Creates a [File] object from a raw path.
+  ///
+  /// A raw path is a sequence of bytes, as paths are represented by the OS.
+  File toFileFromBase64() => File.fromRawPath(base64Decode(this));
 
   /// Parse [source] as a, possibly signed, integer literal.
   ///
@@ -578,8 +678,8 @@ extension ResponseExtension on http.Response {
     String searchBy = '"message":',
     String fallback = 'Error',
   }) {
-    log("$statusCode ⭕");
-    log("$body ⭕");
+    dev.log("$statusCode ⭕");
+    dev.log("$body ⭕");
 
     return body.contains(searchBy)
         ? jsonDecode(body)[searchBy]
