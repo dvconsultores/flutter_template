4271d69097b858932f5ab22c143aa87589c3aa5f
detextre4 <detextre4@gmail.com>
2023-11-10 01:42:06 -0400

    updated

--- lib/widgets/input_field.dart
+++ lib/widgets/input_field.dart
@@ -10,6 +10,7 @@ class InputField extends StatelessWidget {
     this.labelText,
     this.hintText,
     this.disabled = false,
+    this.readOnly = false,
     this.textAlign = TextAlign.start,
     this.keyboardType,
     this.prefixIcon,
@@ -35,12 +36,16 @@ class InputField extends StatelessWidget {
     this.borderRadius = const BorderRadius.all(Radius.circular(40)),
     this.borderWidth = 1,
     this.borderColor = Colors.black,
+    this.floatingLabelBehavior = FloatingLabelBehavior.never,
+    this.contentPadding,
+    this.height,
   });
   final AutovalidateMode? autovalidateMode;
   final TextEditingController? controller;
   final String? labelText;
   final String? hintText;
   final bool disabled;
+  final bool readOnly;
   final TextAlign textAlign;
   final TextInputType? keyboardType;
   final Widget? prefixIcon;
@@ -65,89 +70,44 @@ class InputField extends StatelessWidget {
   final BorderRadius borderRadius;
   final double borderWidth;
   final Color borderColor;
-
-  @override
-  Widget build(BuildContext context) {
-    final textStyle = TextStyle(
-      color: Colors.black45,
-      fontSize: 12,
-      fontWeight: FontWeight.w400,
-      fontFamily: FontFamily.lato("400"),
-    );
-
-    final border = OutlineInputBorder(
-      borderSide: BorderSide(color: borderColor, width: borderWidth),
-      borderRadius: borderRadius,
-    );
-
-    return TextFormField(
-        onTapOutside: onTapOutside,
-        onTap: onTap,
-        focusNode: focusNode,
-        obscureText: obscureText ?? false,
-        controller: controller,
-        enabled: !disabled,
-        textAlign: textAlign,
-        keyboardType: numeric
-            ? const TextInputType.numberWithOptions(signed: true, decimal: true)
-            : keyboardType,
-        maxLines: maxLines,
-        maxLength: maxLength,
-        onChanged: onChanged,
-        validator: validator,
-        autovalidateMode: autovalidateMode,
-        inputFormatters: [
-          if (numeric) ...[
-            DecimalTextInputFormatter(
-              formatByComma: formatByComma,
-              maxEntires: maxEntires,
-              maxDecimals: maxDecimals,
-            ),
-          ],
-          if (inputFormatters != null && inputFormatters!.isNotEmpty)
-            ...inputFormatters!
-        ],
-        decoration: InputDecoration(
-          prefixIconConstraints: BoxConstraints(maxWidth: maxWidthPrefix),
-          hintText: hintText,
-          hintStyle: textStyle,
-          labelText: labelText,
-          labelStyle: textStyle,
-          filled: true,
-          fillColor: ThemeApp.colors(context).tertiary,
-          border: border,
-          enabledBorder: border,
-          disabledBorder: border,
-          prefix: prefix,
-          prefixIcon: prefixIcon != null
-              ? IntrinsicWidth(
-                  child: Padding(
-                    padding: prefixPadding ??
-                        const EdgeInsets.symmetric(horizontal: 10),
-                    child: prefixIcon,
-                  ),
-                )
-              : null,
-          suffix: suffix,
-          suffixIcon: suffixIcon,
-        ));
-  }
+  final FloatingLabelBehavior floatingLabelBehavior;
+  final EdgeInsets? contentPadding;
+  final double? height;
 
   static InputField textBox({
     TextEditingController? controller,
     String? labelText,
     String? hintText,
     bool disabled = false,
+    bool readOnly = false,
+    bool numeric = false,
     TextAlign textAlign = TextAlign.start,
     TextInputType? keyboardType,
     Widget? prefixIcon,
     Widget? suffixIcon,
-    required int maxLines,
+    required int? maxLines,
     int? maxLength,
     FocusNode? focusNode,
     void Function(String value)? onChanged,
     void Function()? onTap,
     void Function(PointerDownEvent event)? onTapOutside,
+    AutovalidateMode? autovalidateMode,
+    Widget? prefix,
+    Widget? suffix,
+    String? Function(String? value)? validator,
+    List<TextInputFormatter>? inputFormatters,
+    bool? obscureText,
+    EdgeInsetsGeometry? prefixPadding,
+    BorderRadius borderRadius = const BorderRadius.all(Radius.circular(40)),
+    double borderWidth = 1,
+    Color borderColor = Colors.black,
+    FloatingLabelBehavior floatingLabelBehavior = FloatingLabelBehavior.never,
+    EdgeInsets? contentPadding,
+    bool formatByComma = true,
+    int maxEntires = 4,
+    int maxDecimals = 2,
+    double maxWidthPrefix = double.infinity,
+    double? height,
   }) =>
       InputField(
         onTapOutside: onTapOutside,
@@ -157,12 +117,121 @@ class InputField extends StatelessWidget {
         controller: controller,
         hintText: hintText,
         disabled: disabled,
-        keyboardType: keyboardType,
+        keyboardType: height != null ? TextInputType.multiline : keyboardType,
         labelText: labelText,
         maxLines: maxLines,
         maxLength: maxLength,
         prefixIcon: prefixIcon,
         suffixIcon: suffixIcon,
         textAlign: textAlign,
+        autovalidateMode: autovalidateMode,
+        borderColor: borderColor,
+        borderRadius: borderRadius,
+        borderWidth: borderWidth,
+        contentPadding: contentPadding,
+        floatingLabelBehavior: floatingLabelBehavior,
+        formatByComma: formatByComma,
+        inputFormatters: inputFormatters,
+        maxDecimals: maxDecimals,
+        maxEntires: maxEntires,
+        maxWidthPrefix: maxWidthPrefix,
+        numeric: numeric,
+        obscureText: obscureText,
+        prefix: prefix,
+        prefixPadding: prefixPadding,
+        readOnly: readOnly,
+        suffix: suffix,
+        validator: validator,
+        height: height,
       );
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(
+      color: Colors.black87,
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+      fontFamily: FontFamily.lato("400"),
+    );
+
+    final hintStyle = textStyle.copyWith(color: Colors.black, fontSize: 12);
+
+    final labelStyle = textStyle.copyWith(color: Colors.black, fontSize: 12);
+
+    final floatingLabelStyle = labelStyle;
+
+    final border = OutlineInputBorder(
+      borderSide: BorderSide(color: borderColor, width: borderWidth),
+      borderRadius: borderRadius,
+    );
+
+    final expanded = height != null;
+
+    return SizedBox(
+      height: height,
+      child: TextFormField(
+          onTapOutside: onTapOutside,
+          onTap: onTap,
+          focusNode: focusNode,
+          obscureText: obscureText ?? false,
+          controller: controller,
+          enabled: !disabled,
+          readOnly: readOnly,
+          textAlign: textAlign,
+          keyboardType: numeric
+              ? const TextInputType.numberWithOptions(
+                  signed: true, decimal: true)
+              : expanded && keyboardType == null
+                  ? TextInputType.text
+                  : keyboardType,
+          maxLines: expanded ? null : maxLines,
+          maxLength: maxLength,
+          onChanged: onChanged,
+          validator: validator,
+          autovalidateMode: autovalidateMode,
+          inputFormatters: [
+            if (numeric) ...[
+              DecimalTextInputFormatter(
+                formatByComma: formatByComma,
+                maxEntires: maxEntires,
+                maxDecimals: maxDecimals,
+              ),
+            ],
+            if (inputFormatters != null && inputFormatters!.isNotEmpty)
+              ...inputFormatters!
+          ],
+          style: textStyle,
+          expands: expanded,
+          decoration: InputDecoration(
+            prefixIconConstraints: BoxConstraints(maxWidth: maxWidthPrefix),
+            enabled: !disabled,
+            hintText: hintText,
+            hintStyle: hintStyle,
+            labelText: labelText,
+            labelStyle: labelStyle,
+            floatingLabelStyle: floatingLabelStyle,
+            floatingLabelBehavior: floatingLabelBehavior,
+            filled: true,
+            fillColor: ThemeApp.colors(context).tertiary,
+            border: border,
+            enabledBorder: border,
+            disabledBorder: border,
+            prefix: prefix,
+            prefixIcon: prefixIcon != null
+                ? IntrinsicWidth(
+                    child: Padding(
+                      padding: prefixPadding ??
+                          const EdgeInsets.symmetric(horizontal: 10),
+                      child: prefixIcon,
+                    ),
+                  )
+                : null,
+            suffix: suffix,
+            suffixIcon: suffixIcon,
+            isDense: true,
+            contentPadding: contentPadding ??
+                const EdgeInsets.symmetric(horizontal: 26, vertical: 10),
+          )),
+    );
+  }
 }
