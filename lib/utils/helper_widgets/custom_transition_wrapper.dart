aef42143cdcc98d2e529967a0ba5059f0ba97e54
detextre4 <detextre4@gmail.com>
2023-11-11 18:18:43 -0400

    fix

--- lib/utils/helper_widgets/custom_transition_wrapper.dart
+++ lib/utils/helper_widgets/custom_transition_wrapper.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:wallet_p2p/utils/helper_widgets/circle_clipper.dart';
+import 'package:flutter_detextre4/utils/helper_widgets/circle_clipper.dart';
 
 Animation<double> _animation(BuildContext context) =>
     ModalRoute.of(context)!.animation!;
