8db9530d34aeadc6b144941af41323ad4ed23c95
detextre4 <detextre4@gmail.com>
2023-11-14 18:39:28 -0400

    added tooltip to app

---
+++ lib/widgets/tooltip.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_detextre4/utils/config/theme.dart';
+
+class AppTooltip extends StatelessWidget {
+  const AppTooltip({
+    super.key,
+    required this.child,
+    this.message,
+  });
+  final Widget child;
+  final String? message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Tooltip(
+      message: message,
+      triggerMode: TooltipTriggerMode.longPress,
+      decoration: BoxDecoration(
+        color: ThemeApp.colors(context).primary,
+        borderRadius: const BorderRadius.all(Radius.circular(8)),
+      ),
+      textStyle: TextStyle(
+        fontSize: 12,
+        color: Colors.white,
+        fontWeight: FontWeight.w400,
+        fontFamily: FontFamily.lato("400"),
+      ),
+      child: child,
+    );
+  }
+}
