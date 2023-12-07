5361a25551e9c5d63882be136a42c203fcd615bb
detextre4 <detextre4@gmail.com>
2023-12-07 15:11:37 -0400

    fixed localAuth service

--- lib/utils/services/local_auth_service.dart
+++ lib/utils/services/local_auth_service.dart
@@ -1,50 +1,27 @@
-import 'dart:io' as io;
-
-import 'package:flutter/foundation.dart';
 import 'package:local_auth/local_auth.dart';
 
-// ! ⚠️under revision⚠️
 class LocalAuth {
-  static final LocalAuthentication _localAuth = LocalAuthentication();
+  /// A Flutter plugin for authenticating the user identity locally.
+  static final auth = LocalAuthentication();
 
-  /// hasBiometrics()
-  ///
-  /// @returns [true] if device has fingerprint/faceID available and registered, [false] otherwise
+  /// @returns [true] if has biometrics, [false] otherwise
   static Future<bool> hasBiometrics() async {
-    if (kIsWeb && !(io.Platform.isAndroid || io.Platform.isIOS)) return false;
-
-    final bool canCheck = await _localAuth.canCheckBiometrics;
-    if (!canCheck) return false;
-
     final List<BiometricType> availableBiometrics =
-        await _localAuth.getAvailableBiometrics();
+        await auth.getAvailableBiometrics();
 
-    if (availableBiometrics.contains(BiometricType.face) ||
-        availableBiometrics.contains(BiometricType.fingerprint) ||
-        availableBiometrics.contains(BiometricType.strong) ||
-        availableBiometrics.contains(BiometricType.weak)) {
-      return true;
-    }
-
-    return false;
+    return availableBiometrics.isNotEmpty;
   }
 
-  /// authenticateWithBiometrics()
-  ///
   /// @param [message] Message shown to user in FaceID/TouchID popup
   /// @returns [true] if successfully authenticated, [false] otherwise
-  static Future<bool> authenticateWithBiometrics() async {
-    const String message = 'Favor de autenticarse para acceder';
-    final bool hasBiometricsEnrolled = await hasBiometrics();
+  static Future<bool> authenticate([String? message]) async {
+    message ??= "Verifica tu identidad antes de continuar";
 
-    if (!hasBiometricsEnrolled) return false;
+    final List<BiometricType> availableBiometrics =
+        await auth.getAvailableBiometrics();
+    if (availableBiometrics.isEmpty) return false;
 
-    final LocalAuthentication localAuth = LocalAuthentication();
-    return await localAuth.authenticate(
-        localizedReason: message,
-        options: const AuthenticationOptions(
-          useErrorDialogs: false,
-          biometricOnly: true,
-        ));
+    // Here ask for bio:
+    return await auth.authenticate(localizedReason: message);
   }
 }
