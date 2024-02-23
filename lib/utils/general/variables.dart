47f5bf32da94debadabd40c3d5592eab108c90cb
detextre4 <detextre4@gmail.com>
2024-02-23 13:46:30 -0400

    update

--- lib/utils/general/variables.dart
+++ lib/utils/general/variables.dart
@@ -4,7 +4,18 @@ import 'package:flutter/material.dart';
 mixin Variables {
   // * fetching
   static const requestTiming = 10;
+
   // * Sizing
   static const mSize = Size(360, 690);
-  static const paddingScaffold = EdgeInsets.all(12.0);
+  static const paddingScaffold = EdgeInsets.symmetric(
+    vertical: 16,
+    horizontal: 24,
+  );
+
+  static const double gapXLow = 2,
+      gapLow = 4,
+      gapMedium = 8,
+      gapLarge = 12,
+      gapXLarge = 16,
+      gapMax = 20;
 }
