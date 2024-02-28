75a59cba4cab246e522be75edf49714b3a5e6580
detextre4 <detextre4@gmail.com>
2024-02-27 22:33:08 -0400

    updates

--- lib/utils/helper_widgets/scroll_animation_builder.dart
+++ lib/utils/helper_widgets/scroll_animation_builder.dart
@@ -1,47 +1,53 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
-class ScrollAnimationBuilder<T> extends StatefulWidget {
+class ScrollAnimationBuilder extends StatefulWidget {
   const ScrollAnimationBuilder({
     super.key,
     required this.controller,
     required this.builder,
-    required this.onListen,
-    this.threshold = 10,
+    this.threshold = 5,
   });
   final ScrollController controller;
-  final Widget Function(BuildContext context, AsyncSnapshot<T> snapshot)
-      builder;
-  final T Function(double offset, ScrollAnimationDirection scrollDirection)
-      onListen;
+  final Widget Function(
+    BuildContext context,
+    ScrollAnimationOptions options,
+  ) builder;
   final double threshold;
 
   @override
-  State<ScrollAnimationBuilder> createState() =>
-      _ScrollAnimationWidgetState<T>();
+  State<ScrollAnimationBuilder> createState() => _ScrollAnimationWidgetState();
+}
+
+class ScrollAnimationOptions {
+  const ScrollAnimationOptions(
+    this.offset,
+    this.scrollDirection,
+  );
+  final double offset;
+  final ScrollAnimationDirection scrollDirection;
 }
 
 enum ScrollAnimationDirection {
-  up,
-  down;
+  leading,
+  trailling;
 }
 
-class _ScrollAnimationWidgetState<T> extends State<ScrollAnimationBuilder<T>> {
-  final animStreamController = StreamController<T>();
+class _ScrollAnimationWidgetState extends State<ScrollAnimationBuilder> {
   double lastOffsetProcessed = 0.0;
 
+  double offset = 0;
+  var scrollDirection = ScrollAnimationDirection.leading;
+
   void listenerScroll() {
-    final offset = widget.controller.offset;
+    offset = widget.controller.offset;
     if ((offset - lastOffsetProcessed).abs() < widget.threshold) return;
 
-    final scrollDirection = offset < lastOffsetProcessed
-        ? ScrollAnimationDirection.up
-        : ScrollAnimationDirection.down;
+    scrollDirection = offset < lastOffsetProcessed
+        ? ScrollAnimationDirection.leading
+        : ScrollAnimationDirection.trailling;
 
     lastOffsetProcessed = offset;
-
-    animStreamController.add(widget.onListen(offset, scrollDirection));
+    setState(() {});
   }
 
   @override
@@ -57,10 +63,63 @@ class _ScrollAnimationWidgetState<T> extends State<ScrollAnimationBuilder<T>> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<T>(
-      stream: animStreamController.stream,
-      builder: widget.builder,
-    );
+  Widget build(BuildContext context) =>
+      widget.builder(context, ScrollAnimationOptions(offset, scrollDirection));
+}
+
+class ScrollAnimationBuilderV2<T> extends StatefulWidget {
+  const ScrollAnimationBuilderV2({
+    super.key,
+    required this.controller,
+    this.initialValue,
+    required this.builder,
+    required this.onListen,
+    this.threshold = 5,
+  });
+  final ScrollController controller;
+  final List<T>? initialValue;
+  final Widget Function(BuildContext context, List<T>? snapshot) builder;
+  final List<T> Function(ScrollAnimationOptions) onListen;
+  final double threshold;
+
+  @override
+  State<ScrollAnimationBuilderV2> createState() =>
+      _ScrollAnimationWidgetStateV2<T>();
+}
+
+class _ScrollAnimationWidgetStateV2<T>
+    extends State<ScrollAnimationBuilderV2<T>> {
+  double lastOffsetProcessed = 0.0;
+
+  List<T>? values;
+
+  void listenerScroll() {
+    final offset = widget.controller.offset;
+    if ((offset - lastOffsetProcessed).abs() < widget.threshold) return;
+
+    final scrollDirection = offset < lastOffsetProcessed
+        ? ScrollAnimationDirection.leading
+        : ScrollAnimationDirection.trailling;
+
+    lastOffsetProcessed = offset;
+
+    values = widget.onListen(ScrollAnimationOptions(offset, scrollDirection));
+    setState(() {});
   }
+
+  @override
+  void initState() {
+    values = widget.initialValue;
+    widget.controller.addListener(listenerScroll);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    widget.controller.removeListener(listenerScroll);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => widget.builder(context, values);
 }
