69c2344592445aa230fbd71008a8995c3fe8da4d
detextre4 <detextre4@gmail.com>
2024-03-15 02:10:29 -0400

    added safe_request widget

---
+++ lib/utils/general/safe_request.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_detextre4/widgets/dialogs/alert_to_continue.dart';
+import 'package:flutter_detextre4/widgets/loaders/loader.dart';
+
+class SafeRequest {
+  static Future<T?> retryOnFailure<T>(
+    Future<T> Function() future, {
+    int maxAttempts = 3,
+    Duration delay = Durations.medium2,
+  }) async {
+    for (int attempt = 0; attempt < maxAttempts; attempt++) {
+      try {
+        return await future();
+      } catch (error) {
+        debugPrint("$error ⭕");
+        await Future.delayed(delay);
+      }
+    }
+
+    return null;
+  }
+
+  static Future<T?> retryOnFailureWithRequest<T>(
+    BuildContext context,
+    Future<T> Function() future, {
+    AppLoader? loader,
+    int maxAttempts = 0,
+    Duration delay = Durations.long2,
+    String title = "Something went wrong",
+    String? contentText,
+    String acceptText = "Retry",
+    String cancelText = "Cancel",
+  }) async {
+    final load = loader ?? AppLoader(context);
+    int attempt = 0;
+
+    while (maxAttempts == 0 || attempt < maxAttempts) {
+      try {
+        load.init();
+        final response = await future();
+        load.close();
+
+        return response;
+      } catch (error) {
+        load.close();
+        if (!context.mounted) return null;
+
+        final accepts = await showDialog(
+          context: context,
+          builder: (context) => AlertToContinue(
+            title: title,
+            content: contentText ?? error.toString(),
+            acceptText: acceptText,
+            cancelText: cancelText,
+          ),
+        );
+        if (accepts != true || !context.mounted) return null;
+        await Future.delayed(delay);
+      }
+
+      attempt++;
+    }
+
+    return null;
+  }
+}
---
+++ lib/widgets/dialogs/alert_to_continue.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_detextre4/utils/general/variables.dart';
+import 'package:flutter_detextre4/widgets/defaults/button.dart';
+import 'package:flutter_gap/flutter_gap.dart';
+
+class AlertToContinue extends StatelessWidget {
+  const AlertToContinue({
+    super.key,
+    this.title,
+    this.content,
+    this.acceptText,
+    this.cancelText,
+  });
+  final String? title;
+  final String? content;
+  final String? acceptText;
+  final String? cancelText;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(Vars.radius40)),
+        ),
+        title: title != null ? Text(title!, textAlign: TextAlign.center) : null,
+        content: content != null
+            ? Text(
+                content!,
+                style: const TextStyle(
+                  color: Color.fromARGB(255, 100, 93, 93),
+                ),
+              )
+            : null,
+        titlePadding: Vars.paddingScaffold
+            .add(const EdgeInsets.only(top: Vars.gapNormal)),
+        contentPadding: EdgeInsets.only(
+          left: Vars.paddingScaffold.left,
+          right: Vars.paddingScaffold.left,
+        ),
+        actionsPadding: Vars.paddingScaffold
+            .add(const EdgeInsets.only(bottom: Vars.gapNormal)),
+        actions: [
+          Row(children: [
+            Expanded(
+                child: Button.variant(
+              text: cancelText ?? "Cancel",
+              onPressed: () => Navigator.pop(context),
+            )),
+            const Gap(Vars.gapXLarge).row,
+            Expanded(
+                child: Button(
+              text: acceptText ?? "Continue",
+              onPressed: () => Navigator.pop(context, true),
+            )),
+          ]),
+        ]);
+  }
+}
