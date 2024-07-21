2630627c12e87292980303961376134bf8c5edc0
detextre4 <detextre4@gmail.com>
2024-07-21 13:03:09 -0400

    fix/deeplink-config

--- android/app/src/main/kotlin/com/example/prod/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/prod/MainActivity.kt
@@ -4,25 +4,9 @@ import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 import io.flutter.embedding.engine.FlutterEngine
 import androidx.annotation.NonNull;
-// import android.content.Intent
-// import android.net.Uri
-// import android.os.Bundle
 
 class MainActivity: FlutterFragmentActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
     }
-
-    //? 🖊️ App linking 🖊️
-    // override fun onCreate(savedInstanceState: Bundle?) {
-    //     super.onCreate(savedInstanceState)
-
-    //     // playstore deep link setting
-    //     val intent = Intent(Intent.ACTION_VIEW).apply {
-    //         data = Uri.parse("https://play.google.com/store/apps/details?id=com.example.prod")
-    //         setPackage("com.android.vending")
-    //     }
-
-    //     startActivity(intent)
-    // }
 }
