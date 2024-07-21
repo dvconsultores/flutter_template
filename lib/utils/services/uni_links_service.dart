f075dcad8f30badd461b556352a59b8ae2e8b49d
detextre4 <detextre4@gmail.com>
2024-07-21 13:06:23 -0400

    fix/unilink

--- lib/utils/services/uni_links_service.dart
+++ lib/utils/services/uni_links_service.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:uni_links/uni_links.dart';
 
 enum UniLinksKey {
@@ -9,6 +9,12 @@ enum UniLinksKey {
 
   const UniLinksKey(this.value);
   final String? value;
+}
+
+enum UniLinksTypeHandler {
+  initialUrl,
+  streamUrl,
+  installReferrer;
 }
 
 class UniLinksService {
@@ -19,45 +25,55 @@ class UniLinksService {
     _sub = null;
   }
 
+  /// Initialization method to [UniLinksService]. Just run once.
   static Future<void> init({checkActualVersion = false}) async {
     // This is used for cases when: APP is not running and the user clicks on a link.
     try {
       final Uri? uri = await getInitialUri();
-      _uniLinkHandler(uri);
-    } on PlatformException {
-      if (kDebugMode) {
-        print("(PlatformException) Failed to receive initial uri.");
-      }
-    } on FormatException catch (error) {
-      if (kDebugMode) {
-        print(
-            "(FormatException) Malformed Initial URI received. Error: $error");
-      }
+      uniLinkHandler(uri, UniLinksTypeHandler.initialUrl);
+    } catch (error) {
+      if (kDebugMode) print("UniLinks getInitialUri error: $error ⭕");
     }
 
     // This is used for cases when: APP is already running and the user clicks on a link.
-    _sub = uriLinkStream.listen(_uniLinkHandler, onError: (error) {
-      if (kDebugMode) print('UniLinks onUriLink error: $error');
+    _sub = uriLinkStream
+        .listen((value) => uniLinkHandler(value, UniLinksTypeHandler.streamUrl),
+            onError: (error) {
+      if (kDebugMode) print('UniLinks onUriLink error: $error ⭕');
     });
   }
 
-  static Future<void> _uniLinkHandler(Uri? uri) async {
-    if (uri == null || uri.queryParameters.isEmpty) return;
+  /// handler to manage uri redirect
+  static bool uniLinkHandler(
+    Uri? uri,
+    UniLinksTypeHandler uniLinksTypeHandler,
+  ) {
+    if (uri == null || uri.queryParameters.isEmpty) return false;
     final params = uri.queryParameters;
 
     for (final key in UniLinksKey.values) {
       final paramValue = params[key.name];
-      if (paramValue == null) return;
+      if (paramValue == null) continue;
 
-      uniLinksActions(key, paramValue);
+      _uniLinksActions(key, paramValue, uniLinksTypeHandler);
+      return true;
     }
+
+    return false;
   }
 
-  static void uniLinksActions(UniLinksKey key, String value) {
-    switch (key) {
-      case UniLinksKey.testKey:
-        if (kDebugMode) print("test key");
-        break;
-    }
+  /// handler to manage uri redirect
+  static void _uniLinksActions(
+    UniLinksKey key,
+    String value,
+    UniLinksTypeHandler uniLinksTypeHandler,
+  ) {
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      switch (key) {
+        case UniLinksKey.testKey:
+          if (kDebugMode) print("test key");
+          break;
+      }
+    });
   }
 }
