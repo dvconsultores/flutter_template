0f0c231a6ba9f3dc338b168aa0fa7af3b106567c
detextre4 <detextre4@gmail.com>
2024-08-09 13:34:47 -0400

    feat/mixin

--- lib/utils/mixins/MIXINS.md
+++
@@ -1,1 +0,0 @@
-# this folder is created to add mixins inside
---
+++ lib/utils/mixins/themes_mixin.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_detextre4/utils/config/theme.dart';
+import 'package:flutter_detextre4/utils/general/context_utility.dart';
+
+mixin ThemesMixin {
+  static final context = ContextUtility.context!;
+
+  final colors = ThemeApp.colors(context),
+      theme = Theme.of(context),
+      media = MediaQuery.of(context);
+}
